5a32a516bec8954a83780a4874f0a794050a608f
rajuganteng <kakakriswa@gmail.com>
2025-03-17 10:47:03 +0700

    Add files via upload

---
+++ mahasiswa.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Data Mahasiswa</title>
+    </head>
+    <body>
+        <h6>Data Mahasiswa AMP</h6>
+        <hr>
+        <h6>NIM: 111111</h6><br>
+        <h6>Nama: rajuganteng </h6><br>
+        <a href="index.php">Kembali</a>
+    </body>
+</html>
